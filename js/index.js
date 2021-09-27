20093f1f1389db3713efde97418ee2f98e87992c
dvoranii <dvoranii@sheridancollege.ca>
2021-09-27 16:15:30 -0400

    copy email function added

--- js/index.js
+++ js/index.js
@@ -14,11 +14,11 @@ navLinks.forEach(link =>{
 let copyText = document.querySelector(".copy-text");
 copyText.querySelector("button").addEventListener("click", function(){
     let input = copyText.querySelector("input.text");
-    input.ariaSelected();
+    input.select();
     document.execCommand("copy");
     copyText.classList.add("active");
     window.getSelection().removeAllRanges();
     setTimeout(function(){
         copyText.classList.remove("active");
-    }, 2500);
+    }, 2500)
 });
